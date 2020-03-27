b2e8ad672ddd5b4dc3f7307168c8edd8aa2f9f7a
Joshua Moore <jmjoshua7465@gmail.com>
2020-03-26 18:34:22 -0600

    Part of the table is being populated! Also removing node.

--- index.js
+++ index.js
@@ -12,14 +12,45 @@ xhr.addEventListener("readystatechange", function () {
 	}
 });
 
-xhr.open("GET", "https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats?country=US");
+xhr.open("GET", "https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats?country=Canada");
 xhr.setRequestHeader("x-rapidapi-host", "covid-19-coronavirus-statistics.p.rapidapi.com");
 xhr.setRequestHeader("x-rapidapi-key", "2f865679d3msh8d12b01bc987b18p1fd323jsnd5ec6e7da75f");
 
 xhr.send(data);
 
+var tabBody;
+
 function populateTable(covidStats) {
+    // Set the first row in the table
+    if (!document.getElementsByClassName) return;
+    tabBody=document.getElementsByClassName("statsTable").item(0);
+
+    addHeader(["City", "State", "Country", "Confirmed Cases", "Deaths", "Recovered"]);
+
     covidStats.forEach(stat => {
-        console.log(stat.province + ": " + stat.confirmed);
+        // Set each row for the data we receive
+        addRow(stat);
     });
+}
+
+function addHeader(titles) {
+    row=document.createElement("tr");
+    titles.forEach(headerTitle => {
+        cell = document.createElement("td");
+        textnode=document.createTextNode(headerTitle);
+        cell.appendChild(textnode);
+        row.appendChild(cell);
+    });
+    tabBody.appendChild(row);
+}
+
+function addRow(stat) {
+    console.log(stat.city);
+    
+    row=document.createElement("tr");
+    cell = document.createElement("td");
+    textnode=document.createTextNode(stat.city);
+    cell.appendChild(textnode);
+    row.appendChild(cell);
+    tabBody.appendChild(row);
 }
